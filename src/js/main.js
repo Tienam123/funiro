a4bff58b54f457b7811f31462371c82ff2abde2a
Vladyslav <vladyslav.honchar1990@gmail.com>
2024-09-23 02:40:29 +0300

    complete full responcive main hero section with slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,25 @@
 import {isMobile} from "./service/isMobile.js";
 import {removeClasses} from "./service/removeClasses.js";
 import {spollers,menuInit} from './service/functions.js';
-menuInit();
-spollers();
-window.onload = function () {
-    document.addEventListener('click', documentActions);
+import 'swiper/css'
+import Swiper from "swiper";
+import {Navigation, Pagination} from "swiper/modules";
 
+const header = document.querySelector('header');
+
+window.onload = function () {
+    menuInit();
+    spollers();
+    document.addEventListener('click', documentActions);
+    window.addEventListener('scroll', pageScrolled);
+
+    function pageScrolled(e) {
+        if (window.scrollY > 50) { // Если прокрутили более чем на 50px
+            header.classList.add('header-scrolled'); // Добавляем класс
+        } else {
+            header.classList.remove('header-scrolled'); // Убираем класс
+        }
+    }
     function documentActions(e) {
         const targetElement = e.target;
         if (window.innerWidth > 768 && isMobile.any()) {
@@ -22,10 +36,28 @@ window.onload = function () {
         } else if (!targetElement.closest('search-form') && !document.querySelector('search-form._active')) {
             document.querySelector('.search-form').classList.remove('_active');
         }
-
-
     }
 }
 
-
-
+if (document.querySelector('.slider-main__body')) {
+    const swiper = new Swiper('.slider-main__body', {
+        modules: [Navigation, Pagination],
+        loop: true,
+        direction: "horizontal",
+        observer: true,
+        observeParents: true,
+        slidesPerView: 1,
+        spaceBetween: 32,
+        watchOverflow: true,
+        speed:800,
+        parallax: true,
+        pagination: {
+            el: '.controls-slider-main__dotts',
+            clickable: true
+        },
+        navigation:{
+            nextEl: '.controls-slider-main .slider-arrow-next',
+            prevEl:'.controls-slider-main .slider-arrow-prev'
+        }
+    });
+}
